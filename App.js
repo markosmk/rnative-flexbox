45bba2de214a182f1aa3d7f6d32ad47b29248c37
Markos <markosmk@me.com>
2021-11-17 11:00:10 -0300

    [add] layout flexbox

--- App.js
+++ App.js
@@ -1,21 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ScrollView>
+        <View style={{ flexDirection: 'row' }}>
+          <Image style={styles.banner} source={require('./assets/img/bg.jpg')} />
+        </View>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Que hacer en Paris!</Text>
+          <ScrollView horizontal>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad3.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad4.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad5.jpg')}
+              />
+            </View>
+          </ScrollView>
+          <Text style={styles.titulo}>Los Mejores Alojamientos</Text>
+          <View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/mejores3.jpg')}
+              />
+            </View>
+          </View>
+          <Text style={styles.titulo}>Hospedajes en Los Angeles</Text>
+          <View style={styles.listado}>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/hospedaje1.jpg')}
+              />
+              <Text style={styles.listadoText}>Casa para Audiciones</Text>
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/hospedaje2.jpg')}
+              />
+              <Text style={styles.listadoText}>Casa para Minimalistas</Text>
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/hospedaje3.jpg')}
+              />
+              <Text style={styles.listadoText}>Casa para Familias</Text>
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require('./assets/img/hospedaje4.jpg')}
+              />
+              <Text style={styles.listadoText}>Casa Coloquial</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  banner: {
+    height: 250,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titulo: {
+    fontWeight: 700,
+    fontSize: 22,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
+  },
+  mejores: {
+    width: '100%',
+    height: 200,
+    marginVertical: 5,
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  listadoItem: {
+    flexBasis: '49%',
+  },
+  listadoText: {
+    fontSize: 16,
+    fontWeight: 500,
+    marginBottom: 10,
   },
 });
